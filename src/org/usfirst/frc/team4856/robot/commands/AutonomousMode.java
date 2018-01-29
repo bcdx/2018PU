53dcb508a1af9ff1e365de549de755428ac710cb
computer1 <pakavoor@chapin.edu>
2018-01-29 18:58:32 -0500

    autonomous. everything works.

--- src/org/usfirst/frc/team4856/robot/commands/AutonomousMode.java
+++ src/org/usfirst/frc/team4856/robot/commands/AutonomousMode.java
@@ -1,99 +1,96 @@
-//package org.usfirst.frc.team4856.robot.commands;
-//import org.usfirst.frc.team4856.robot.Robot;
-//import org.usfirst.frc.team4856.robot.subsystems.Gyro;
-//import org.usfirst.frc.team4856.robot.subsystems.Scoop;
-//
-////CAN Talon support package
-//import com.ctre.CANTalon;
-//
-//import edu.wpi.first.wpilibj.AnalogGyro;
-//import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.command.Command;
-//import edu.wpi.first.wpilibj.command.CommandGroup;
-//import edu.wpi.first.wpilibj.command.Scheduler;
-//
-///**
-// *
-// */
-//public class AutonomousMode extends CommandGroup {
-//
-//	CANTalon left1= new CANTalon(0);
-//	CANTalon left2= new CANTalon(1);
-//	CANTalon right1= new CANTalon(2);
-//	CANTalon right2= new CANTalon(3);
-//
-//	Timer timer;
-//	Scoop scoop;
-//	//Gyro gyro;
-//	
-//    public AutonomousMode() {
-//    	timer = new Timer();
-////    	scoop = new Scoop(0, 1);
-//    	//gyro = new Gyro(1);
-//    	
-////    	 Scheduler.getInstance().run();
-//// drives forward for certain amount of time
-////    	 long t= System.currentTimeMillis();
-////    	 long end = t+5000;
-////    	 while(System.currentTimeMillis() < end){
-////    		 left1.set(0.45);
-////             left2.changeControlMode(CANTalon.TalonControlMode.Follower);
-////             left2.set(left1.getDeviceID());
-////             right1.set(-0.5);
-////             right2.changeControlMode(CANTalon.TalonControlMode.Follower);
-////             right2.set(right1.getDeviceID()); 
-////    	 	}   
-//             
-////             left1.setSafetyEnabled(true); //hopefully will stop motors after 3 sec 2/26 AT
-////             right1.setSafetyEnabled(true);
-////             left1.setExpiration(3.0);
-////             left1.set(0);
-////             right1.setExpiration(3.0);
-////             right1.set(0);
-//    }
-//
-//    // Called just before this Command runs the first time
-//    protected void initialize() {
-//    	timer.reset();
-//    	timer.start();
-//    }
-//
-//    // Called repeatedly when this Command is scheduled to run
-//    protected void execute() {
-//    	//scoop.up(); 
-////    //	Timer.delay(3);
-////    	//scoop.stop(); 
-////    	if (timer.get() < 7){
-////    		left1.set(0.365);
-////    		left2.set(0.365);
-////    		right1.set(0.5);
-////    		right2.set(0.5);
-////    	}
-//////    	gyro.turn(30);
-////
-//////    	while (timer.get() > 1){
-////    		System.out.println(gyro.getAngle());
-////    	}
-//    	
-//    }
-//
-//    // Make this return true when this Command no longer needs to run execute()
-//    protected boolean isFinished() {
-//       // return timer.get()> 4;
-//    	return false;
-//    }
-// 
-//    // Called once after isFinished returns true
-//    protected void end() {    
-//        Robot.left1.set(0);
-//        Robot.right1.set(0);
-//        Robot.left2.set(0);
-//        Robot.right2.set(0);
-//    }
-//
-//    // Called when another command which requires one or more of the same
-//    // subsystems is scheduled to run
-//    protected void interrupted() {
-//    	end();
-//    }
-//}
+package org.usfirst.frc.team4856.robot.commands;
+
+import org.usfirst.frc.team4856.robot.Robot;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+
+public class AutonomousMode extends CommandGroup {
+
+	Timer timer;
+	//double speed = 
+//	public void GyroSample() {
+//		AnalogGyro myGyro = new AnalogGyro(1);
+//		double angle = myGyro.getAngle();
+//		System.out.println(angle);
+//	}
+	
+
+
+    public AutonomousMode() {
+    	timer = new Timer();
+  
+        // Use requires() here to declare subsystem dependencies; eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer.reset();
+    	timer.start();
+    	
+		//Robot.drivetrain.left1.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		//Robot.drivetrain.left2.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10); 
+//		//Robot.drivetrain.right1.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10); 
+//		Robot.drivetrain.right2.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		//Robot.drivetrain.left1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10); 
+		//Robot.drivetrain.left2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10); 
+		//Robot.drivetrain.right1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10); 
+		//Robot.drivetrain.right2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+		
+//    	System.out.println("L38");
+	   	Robot.drivetrain.left1.set(ControlMode.PercentOutput, 0.5);
+	    Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0.5);
+	    Robot.drivetrain.right1.set(ControlMode.PercentOutput, -0.5);
+	    Robot.drivetrain.right2.set(ControlMode.PercentOutput, -0.5);
+//    	System.out.println("L43");
+
+    }
+    
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {	
+    	
+		
+//		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//		LocalDateTime now = LocalDateTime.now();
+		Timer.delay(3.0);
+//		System.out.println(dtf.format(now));
+		//System.out.println(Robot.drivetrain.right1.getSelectedSensorPosition(0));        // getSelectedSensorPosition(int pidIdx) 0 for primary closed-loop
+//   	Timer.delay(2.8); move forward (left and right position)
+//		GyroSample();
+//		System.out.println("i am not a failure");
+
+		
+    }
+
+    
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	return timer.get() > 300; //stops autonomous mode when timer is longer than 15 seconds
+    }
+    
+    // Called once after isFinished returns true
+    protected void end() {
+    	System.out.println("end end");
+	   	Robot.drivetrain.left1.set(ControlMode.PercentOutput, 0);
+	    Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0);
+	    Robot.drivetrain.right1.set(ControlMode.PercentOutput, 0);
+	    Robot.drivetrain.right2.set(ControlMode.PercentOutput, 0);
+//	    System.out.println("can i possibly be functional");
+    }
+
+    // Called when another command which requires one or more of the same subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
