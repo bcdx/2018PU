7c0cc9373cdf06da2b85372a081edce461eef96c
computer1 <pakavoor@chapin.edu>
2018-02-04 13:37:41 -0500

    robot turning 90 degrees (ish)

--- src/org/usfirst/frc/team4856/robot/commands/AutonomousMode.java
+++ src/org/usfirst/frc/team4856/robot/commands/AutonomousMode.java
@@ -2,7 +2,12 @@ package org.usfirst.frc.team4856.robot.commands;
 
 import org.usfirst.frc.team4856.robot.Robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.PIDController;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
@@ -14,19 +19,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class AutonomousMode extends CommandGroup {
-
+	public ADXRS450_Gyro gyroSPI = new ADXRS450_Gyro();
 	Timer timer;
-	//double speed = 
-//	public void GyroSample() {
-//		AnalogGyro myGyro = new AnalogGyro(1);
-//		double angle = myGyro.getAngle();
-//		System.out.println(angle);
-//	}
-	
+	double gyro_adj = 0.0;
+	double temp =.4;
+	double k1 = .002;
+	int epsilon = 2;
+
+
 
 
     public AutonomousMode() {
     	timer = new Timer();
+    	
         // Use requires() here to declare subsystem dependencies; eg. requires(chassis);
     }
 
@@ -35,6 +40,12 @@ public class AutonomousMode extends CommandGroup {
     	timer.reset();
     	timer.start();
     	
+    	gyroSPI.reset();
+    	
+    	
+ 
+    	
+    	    	
 		//Robot.drivetrain.left1.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
 		//Robot.drivetrain.left2.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10); 
 		//Robot.drivetrain.right1.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10); 
@@ -44,23 +55,112 @@ public class AutonomousMode extends CommandGroup {
 		//Robot.drivetrain.right1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10); 
 		//Robot.drivetrain.right2.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 		
-	   	Robot.drivetrain.left1.set(ControlMode.PercentOutput, 0.3);
-	    Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0.3);
-	    Robot.drivetrain.right1.set(ControlMode.PercentOutput, -0.3);
-	    Robot.drivetrain.right2.set(ControlMode.PercentOutput, -0.3);
+	   	Robot.drivetrain.left1.set(ControlMode.PercentOutput, temp);
+	    Robot.drivetrain.left2.set(ControlMode.PercentOutput, temp);
+	    Robot.drivetrain.right1.set(ControlMode.PercentOutput, -temp);
+	    Robot.drivetrain.right2.set(ControlMode.PercentOutput, -temp);
+		
+	    while (timer.get()<1){ ///outer if loop
+    	
+			Timer.delay(0.05);
+		//		System.out.println(dtf.format(now));
+		  
+		//   	Timer.delay(2.8); move forward (left and right position)
+		//		System.out.println("position: " + Robot.drivetrain.right1.getSelectedSensorPosition(0) + gyroSPI.getAngle()); // getSelectedSensorPosition(int pidIdx) 0 for primary closed-loop
+			if (gyroSPI.getAngle()>1) { //inner if/else statements that make the robot drive straight for the first 3 seconds
+				gyro_adj+=-k1;
+			}
+			else if (gyroSPI.getAngle()<-1){
+				gyro_adj+=k1;
+			}
+			else{
+				gyro_adj=0;
+			}
+			
+			Robot.drivetrain.left1.set(ControlMode.PercentOutput, temp+gyro_adj);
+		    Robot.drivetrain.left2.set(ControlMode.PercentOutput, temp+gyro_adj);
+			System.out.println("angle:" + gyroSPI.getAngle());  
+		    System.out.println("gyro adjustment:" + gyro_adj);
+		}
+		while (gyroSPI.getAngle()<90){ //outer end of if loop. Hopefully makes robot turn 90 degrees
+			Robot.drivetrain.left1.set(ControlMode.PercentOutput,0.15);
+			Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0.15);
+			Robot.drivetrain.right1.set(ControlMode.PercentOutput, 0);
+			Robot.drivetrain.right2.set(ControlMode.PercentOutput, 0);
+			Timer.delay(0.5);
+			System.out.println("angle:" + gyroSPI.getAngle());  
+		    System.out.println("gyro adjustment:" + gyro_adj);
+		}
+		while (timer.get()>1){ ///outer if loop
+			Robot.drivetrain.left1.set(ControlMode.PercentOutput, 0.2);
+		    Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0.2);
+		    Robot.drivetrain.right1.set(ControlMode.PercentOutput, -0.2);
+		    Robot.drivetrain.right2.set(ControlMode.PercentOutput, -0.2);
+//			System.out.println("angle:" + gyroSPI.getAngle());  
+//		    System.out.println("gyro adjustment:" + gyro_adj);
+		}
+	    
+	   	Robot.drivetrain.left1.set(ControlMode.PercentOutput, 0);
+	    Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0);
+	    Robot.drivetrain.right1.set(ControlMode.PercentOutput, 0);
+	    Robot.drivetrain.right2.set(ControlMode.PercentOutput, 0);
 
     }
     
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {	
     	
-		Timer.delay(3.0);
-//		System.out.println(dtf.format(now));
-		System.out.println("position: " + Robot.drivetrain.right1.getSelectedSensorPosition(0));        // getSelectedSensorPosition(int pidIdx) 0 for primary closed-loop
-//   	Timer.delay(2.8); move forward (left and right position)
+//		if (timer.get()<1){ ///outer if loop
+//    	
+//			Timer.delay(0.05);
+//		//		System.out.println(dtf.format(now));
+//		  
+//		//   	Timer.delay(2.8); move forward (left and right position)
+//		//		System.out.println("position: " + Robot.drivetrain.right1.getSelectedSensorPosition(0) + gyroSPI.getAngle()); // getSelectedSensorPosition(int pidIdx) 0 for primary closed-loop
+//			if (gyroSPI.getAngle()>1) { //inner if/else statements that make the robot drive straight for the first 3 seconds
+//				gyro_adj+=-k1;
+//			}
+//			else if (gyroSPI.getAngle()<-1){
+//				gyro_adj+=k1;
+//			}
+//			else{
+//				gyro_adj=0;
+//			}
+//			
+//			Robot.drivetrain.left1.set(ControlMode.PercentOutput, temp+gyro_adj);
+//		    Robot.drivetrain.left2.set(ControlMode.PercentOutput, temp+gyro_adj);
+//			System.out.println("angle:" + gyroSPI.getAngle());  
+//		    System.out.println("gyro adjustment:" + gyro_adj);
+//		}
+//		else if (gyroSPI.getAngle()<90){ //outer end of if loop. Hopefully makes robot turn 90 degrees
+//			Robot.drivetrain.left1.set(ControlMode.PercentOutput,0.15);
+//			Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0.15);
+//			Robot.drivetrain.right1.set(ControlMode.PercentOutput, 0);
+//			Robot.drivetrain.right2.set(ControlMode.PercentOutput, 0);
+//			Timer.delay(0.5);
+//			System.out.println("angle:" + gyroSPI.getAngle());  
+//		    System.out.println("gyro adjustment:" + gyro_adj);
+//		}
+//		else {
+//			Robot.drivetrain.left1.set(ControlMode.PercentOutput,0);
+//			Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0);
+//			Robot.drivetrain.right1.set(ControlMode.PercentOutput, 0);
+//			Robot.drivetrain.right2.set(ControlMode.PercentOutput, 0);	
+//			System.out.println("angle:" + gyroSPI.getAngle());  
+//		    System.out.println("gyro adjustment:" + gyro_adj);
+//		}
+//		if (gyroSPI.getAngle()< (90 + epsilon) &(gyroSPI.getAngle()>(90 - epsilon)){
+//			Robot.drivetrain.left1.set(ControlMode.PercentOutput,0.1);
+//			Robot.drivetrain.left2.set(ControlMode.PercentOutput, 0.1);
+//			Robot.drivetrain.right1.set(ControlMode.PercentOutput, -0.01);
+//			Robot.drivetrain.right2.set(ControlMode.PercentOutput, -0.01);
 		
-    }
-
+    	//}
+//			
+//			
+		}
+    
+           
     
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
